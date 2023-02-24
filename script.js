774c589ab7c444805ac808a58d5a601208038214
Douglas Braz Batista <doug.batista@live.com>
2023-02-23 23:04:27 -0300

    feat: pagina de instruções
    foi criado a pagina de instruções e finalizado a animação das cores da homepage.

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
+const red = document.querySelector(".red");
+const green = document.querySelector(".green");
+const blue = document.querySelector(".blue");
+const yellow = document.querySelector(".yellow");
+
+
+function shuffleOrder () {
+    let colorOrder =  Math.floor(Math.random() *4);
+    console.log (colorOrder)
+
+    if (colorOrder == 0) {        
+        green.classList.add("game__pad--active")
+        yellow.classList.remove("game__pad--active")
+        red.classList.remove("game__pad--active")
+        blue.classList.remove("game__pad--active")
+
+      } 
+      
+      else if (colorOrder == 1) {         
+        yellow.classList.add("game__pad--active")
+        red.classList.remove("game__pad--active")
+        blue.classList.remove("game__pad--active")  
+        green.classList.remove("game__pad--active")
+      } 
+      
+      else if (colorOrder == 2) {        
+        red.classList.add("game__pad--active")
+        blue.classList.remove("game__pad--active")  
+        green.classList.remove("game__pad--active")
+        yellow.classList.remove("game__pad--active")
+      } 
+      
+      else if (colorOrder == 3) {        
+        blue.classList.add("game__pad--active")
+        green.classList.remove("game__pad--active")
+        yellow.classList.remove("game__pad--active")
+        red.classList.remove("game__pad--active")
+      }
+    // colorOrder()  
+    setTimeout(() => {shuffleOrder()}, 700)
+     
+}
+
+window.onload(shuffleOrder())
+
+
+
 function test () {
      let usertxt = document.getElementById("textinput").value;
      localStorage.setItem('user', usertxt)
-
-    console.log(usertxt);
-
-    
-    // testuser.innerHTML = "usertxt";
-
-    // _gui.user.innerHTML = "test";
-
-    // console.log(testuser);
-
-
-    
 }
 
-// _gui.buton.addEventListener("click", () => {
-//    test();
-//    console.log("funcionou")
-   
-// })
